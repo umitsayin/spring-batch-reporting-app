77b249ddceef9f3baeae3d84ad7ff1fd0e4b564b
Ümit Sayın <umitsayin1661@gmail.com>
2023-04-07 13:02:25 +0300

    end batch app

--- src/main/java/com/batchapp/SpringBatchApplication.java
+++ src/main/java/com/batchapp/SpringBatchApplication.java
@@ -1,6 +1,11 @@
 package com.batchapp;
 
+import com.batchapp.api.request.CreateCustomerRequest;
+import com.batchapp.api.request.CreateInvoiceRequest;
+import com.batchapp.service.CustomerService;
+import com.batchapp.service.InvoiceService;
 import com.batchapp.service.JobService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,9 +15,15 @@ import org.springframework.scheduling.annotation.Scheduled;
 @EnableScheduling
 public class SpringBatchApplication {
     private final JobService jobService;
+    private final CustomerService customerService;
+    private final InvoiceService invoiceService;
 
-    public SpringBatchApplication(JobService jobService) {
+    public SpringBatchApplication(JobService jobService,
+                                  CustomerService customerService,
+                                  InvoiceService invoiceService) {
         this.jobService = jobService;
+        this.customerService = customerService;
+        this.invoiceService = invoiceService;
     }
 
     public static void main(String[] args) {
@@ -22,5 +33,24 @@ public class SpringBatchApplication {
     @Scheduled(fixedRate = 300000, initialDelay = 60000)
     public void myTask() {
         jobService.runBatchJob();
+    }
+
+
+    @PostConstruct
+    public void setUp(){
+        CreateCustomerRequest customerRequest = new CreateCustomerRequest();
+        customerRequest.setFirstname("Ümit");
+        customerRequest.setLastname("Sayın");
+        customerRequest.setEmail("umitsayin1661@gmail.com");
+        customerRequest.setPhone("111222333");
+
+        CreateInvoiceRequest invoiceRequest = new CreateInvoiceRequest();
+        invoiceRequest.setCustomerId(1);
+        invoiceRequest.setName("Test invoice");
+        invoiceRequest.setTotalPrice(1000);
+        invoiceRequest.setCurrency("TL");
+
+        customerService.add(customerRequest);
+        invoiceService.add(invoiceRequest);
     }
 }
